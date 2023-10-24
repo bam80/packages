34f8f83a07fe74b3007d4b49bccdc30727f717ed
Andrey Butirsky <butirsky@gmail.com>
2023-10-24 15:45:34 +0000

    Pending changes exported from your codespace

---
+++ build/CMakeFiles/auc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auc.dir/auc.c.o"
+  "CMakeFiles/auc.dir/auc.c.o.d"
+  "auc"
+  "auc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/auc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
